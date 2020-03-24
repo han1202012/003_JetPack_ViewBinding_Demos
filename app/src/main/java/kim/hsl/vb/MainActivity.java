fbfb498068a3b1a62af749033ef040ca24b82350
octopus <octopus_truth@163.com>
2020-03-24 20:24:12 +0800

    update

--- app/src/main/java/kim/hsl/vb/MainActivity.java
+++ app/src/main/java/kim/hsl/vb/MainActivity.java
@@ -4,11 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import kim.hsl.vb.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    /**
+     * 视图绑定类 对象
+     */
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        // 1 . 获取视图绑定类
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+
+        // 2 . 关联视图绑定类 与 Activity
+        setContentView(binding.getRoot());
+
+        // 3 . 使用视图绑定类设置
+        binding.textView.setText("视图绑定 ( ViewBinding ) 示例");
     }
 }
