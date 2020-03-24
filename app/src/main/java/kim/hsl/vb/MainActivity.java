8660dbb548d33e77d28b27895d01dcf4bd3e1a30
octopus <octopus_truth@163.com>
2020-03-24 20:08:29 +0800

    Initial commit

---
+++ app/src/main/java/kim/hsl/vb/MainActivity.java
@@ -0,0 +1,14 @@
+package kim.hsl.vb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
